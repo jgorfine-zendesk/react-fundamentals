ec560b3db0b56b89fed649f2a485052fb647faac
Jen Gorfine <jen.gorfine@zendesk.com>
2023-01-12 10:16:53 -0500

    feat: completed exercise 5 and 5 extras

---
+++ src/exercise/05.extra-1.js
@@ -0,0 +1,42 @@
+// Styling
+// 💯 create a custom component
+// http://localhost:3000/isolated/exercise/05.extra-1.js
+
+import * as React from 'react'
+import '../box-styles.css'
+
+const Box = ({className, style, children}) => {
+  return (
+    <div className={`box ${className}`} style={{...style, fontStyle: 'italic'}}>
+      {children}
+    </div>
+  )
+}
+
+const smallBox = (
+  <Box className="box--small" style={{backgroundColor: 'lightblue'}}>
+    small lightblue box
+  </Box>
+)
+const mediumBox = (
+  <Box className="box--medium" style={{backgroundColor: 'pink'}}>
+    medium pink box
+  </Box>
+)
+const largeBox = (
+  <Box className="box--large" style={{backgroundColor: 'orange'}}>
+    large orange box
+  </Box>
+)
+
+function App() {
+  return (
+    <div>
+      {smallBox}
+      {mediumBox}
+      {largeBox}
+    </div>
+  )
+}
+
+export default App
